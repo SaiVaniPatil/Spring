5013a6fb181262b279287ab36e4752d5cb30e81c
SaiVaniPatil <saivani.patil@zoho.com>
2023-04-21 11:09:26 +0200

    ModelandView instead of session

--- springmvcboot/src/main/java/com/nerchuko/first/HomeController.java
+++ springmvcboot/src/main/java/com/nerchuko/first/HomeController.java
@@ -3,6 +3,7 @@ package com.nerchuko.first;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -19,20 +20,25 @@ public class HomeController
 	}
 	
 	@RequestMapping("add")
-	public String addNum(@RequestParam("num1")int i, @RequestParam("num2") int j, HttpSession session)
+	public ModelAndView addNum(@RequestParam("num1")int i, @RequestParam("num2") int j, HttpSession session)
 	{
 		
 //		int num1 = Integer.parseInt(req.getParameter("num1"));
 //		int num2 = Integer.parseInt(req.getParameter("num2"));
 		//int num3 = num1+num2;
 		
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("result.jsp");
+		
 		int num3 = i+j;
 		
+		mv.addObject("num3", num3);
 		//HttpSession session = req.getSession();
 		
-		session.setAttribute("num3", num3);
+		//session.setAttribute("num3", num3);
 		
-		return "result.jsp";
+		return mv;
 		
 	}
 
