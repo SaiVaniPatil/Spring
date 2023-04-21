1e6301ff15d34622df8c620d995bbe8ad6625827
SaiVaniPatil <saivani.patil@zoho.com>
2023-04-21 11:05:24 +0200

    Request Params

--- springmvcboot/src/main/java/com/nerchuko/first/HomeController.java
+++ springmvcboot/src/main/java/com/nerchuko/first/HomeController.java
@@ -2,6 +2,7 @@ package com.nerchuko.first;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -18,14 +19,16 @@ public class HomeController
 	}
 	
 	@RequestMapping("add")
-	public String addNum(HttpServletRequest req)
+	public String addNum(@RequestParam("num1")int i, @RequestParam("num2") int j, HttpSession session)
 	{
 		
-		int num1 = Integer.parseInt(req.getParameter("num1"));
-		int num2 = Integer.parseInt(req.getParameter("num2"));
-		int num3 = num1+num2;
+//		int num1 = Integer.parseInt(req.getParameter("num1"));
+//		int num2 = Integer.parseInt(req.getParameter("num2"));
+		//int num3 = num1+num2;
 		
-		HttpSession session = req.getSession();
+		int num3 = i+j;
+		
+		//HttpSession session = req.getSession();
 		
 		session.setAttribute("num3", num3);
 		
